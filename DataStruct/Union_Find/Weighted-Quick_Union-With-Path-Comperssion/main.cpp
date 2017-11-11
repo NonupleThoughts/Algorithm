d0975f916977c2292e76f2d2ca8057268c3710d9
NonupleThoughts <wangicong@outlook.com>
2017-11-11 20:06:29 +0800

    add Weighted-Quick_Union-With-Path-Comperssion which is a improved method for Quick_Union

---
+++ DataStruct/Union_Find/Weighted-Quick_Union-With-Path-Comperssion/main.cpp
@@ -0,0 +1,125 @@
+/*
+*   This is improved algorithm for Quick_Union
+*       when two tree(p, q) merging, there is a weight for one add to another(add p to q or add q to p)
+*       when find the root( or group number) of the node, there is a path compression
+*   Writed by Cong
+*   2017.11.11
+*/
+
+#include <iostream>
+
+#define def_num 10
+
+using namespace std;
+
+
+class UF
+{
+public:
+    UF(int n);                                  //initialize N nodes with integer group (0 to N - 1)
+    int  UFfind(int p);                         //return the group of p (0 to N - 1)
+    void UFset(int p, int group);               //set the group of node p
+    bool UFconnected(int p, int q);             //return true if p and q are in the same group
+    void UFunion(int p, int q);                 //add connection between p and q
+    void UFdetail();                            //print the group of the node
+    ~UF();                                      //free memory
+
+private:
+    int *id;
+    int *size;                                  //the size of each tree
+    int num;                                    //the number of node
+};
+
+UF::UF(int n = def_num)
+{
+    id = new int[n];
+    size = new int[n];
+    int count_i = 0;
+    for(count_i = 0; count_i < n; count_i += 1)
+    {
+        id[count_i] = count_i;                   //the element contained in the array p is the group of node
+        size[count_i] = 1;                       //the default size of each tree(root) is 1
+    }
+    num = n;
+    cout << "hello world" <<endl;
+}
+
+int UF::UFfind(int p)
+{
+    //find the root, because the root number is the group number
+    int p_temp = p;
+    while(id[p_temp] != p_temp)
+    {
+        //there will compress the path from the node to the root
+        //change the parent node of this node to it's grandparent node
+        id[p_temp] = id[id[p_temp]];
+
+        p_temp = id[p_temp];
+    }
+
+    return p_temp;
+}
+
+void UF::UFset(int p, int group)
+{
+    id[p] = group;
+}
+
+// it seems that this function is useless
+bool UF::UFconnected(int p, int q)
+{
+    return UFfind(p) == UFfind(q);
+}
+
+void UF::UFunion(int p, int q)
+{
+    int p_id = UFfind(p);
+    int q_id = UFfind(q);
+    if(p_id != q_id)
+    {
+        //the weight is the size of each tree(group)
+        if(size[q_id] <= size[p_id])
+        {
+            id[q_id] = p;
+            size[p_id] += size[q_id];
+        }
+        else
+        {
+            id[p_id] = q;
+            size[q_id] += size[p_id];
+        }
+        
+    }
+}
+
+void UF::UFdetail()
+{
+    for(int i = 0; i < num; i += 1)
+    {
+
+        cout << "node" << i << '\t';
+        cout << "group" << id[i] << endl;
+    }
+}
+
+UF::~UF()
+{
+    delete []id;
+    delete []size;
+    cout << "goodbye" <<endl;
+}
+
+
+int main()
+{
+    UF a;
+    a.UFunion(1,2);
+    a.UFunion(5,1);
+    a.UFunion(5,0);
+
+    a.UFunion(7,9);
+    a.UFunion(9,4);
+    a.UFunion(4,1);
+    a.UFdetail();
+    return 0;
+}
