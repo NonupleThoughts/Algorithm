09bfd22146cf79a0efe01b4f90b335fde53c98f8
NonupleThoughts <wangicong@outlook.com>
2017-12-13 15:39:16 +0800

    finish the frame of template class named linked_list, but the function of it is not finished

--- Sort/InsertSort/linked_list.h
+++ Sort/InsertSort/linked_list.h
@@ -1,20 +1,77 @@
 #ifndef _LINKED_LIST_H_
 #define _LINKED_LIST_H_
 
-class UF
+//the implementation of the function of linked_list is all in this file
+//this is because that "Templates can only be implemented in the header file"
+
+
+#include "iostream"
+
+using std::cout;
+using std::endl;
+
+//i think that the node is no need to set as a class
+//because node is very simple, and it just put one memory which is used to save data and two pointers into a block
+template <class datatype>
+struct node
 {
-public:
-    UF(int n);                                  //initialize N nodes with integer group (0 to N - 1)
-    int  UFfind(int p);                         //return the group of p (0 to N - 1)
-    void UFset(int p, int group);               //set the group of node p
-    bool UFconnected(int p, int q);             //return true if p and q are in the same group
-    void UFunion(int p, int q);                 //add connection between p and q
-    void UFdetail();                            //print the group of the node
-    ~UF();                                      //free memory
+    datatype  data;
+    node     *front;
+    node     *rear;
+    
+};
+
+template <class datatype>
+class linked_list
+{
+public:    
+    linked_list();
+    linked_list(datatype data);
+
+    void show_info();
+    
+    ~linked_list();
+
+protected:
+    node<datatype> head;
+    int  size;
 
 private:
-    ;
-    int num;                                    //the number of node
+
 };
+
+template <class datatype>
+inline linked_list<datatype>::linked_list()
+{
+    head.data = (datatype)0;
+    head.front = NULL;
+    head.rear  = NULL;
+    size = 0;
+}
+
+template <class datatype>
+inline linked_list<datatype>::linked_list(datatype data)
+{
+    head.data = data;
+    head.front = NULL;
+    head.rear  = NULL;
+    size = 1;
+}
+
+template <class datatype>
+inline void linked_list<datatype>::show_info()
+{
+    cout << "data"  << head.data  << '\n'
+         << "front" << head.front << '\n'
+         << "rear"  << head.rear  << '\n'
+         << "size"  << size       << endl;
+}
+
+
+template <class datatype>
+inline linked_list<datatype>::~linked_list()
+{
+
+}
 
 #endif  //_LINKED_LIST_H_
---
+++ Sort/InsertSort/main.cpp
@@ -0,0 +1,12 @@
+#include "iostream"
+#include "linked_list.h"
+
+using namespace std;
+
+int main()
+{
+    linked_list<int> a;
+    a.show_info();
+    cout << "hello world" << endl;
+    return 0;
+}
